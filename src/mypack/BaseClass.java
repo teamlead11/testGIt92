659331d9f2ce057691e3e49921971a7bf7209603
unknown <Gopinathanraman13790>
2018-11-22 23:45:32 +0530

    Palindrome Testcase Added

--- src/mypack/BaseClass.java
+++ src/mypack/BaseClass.java
@@ -1,5 +1,7 @@
 package mypack;
 
+import java.util.Scanner;
+
 public class BaseClass {
 
 	public static void main(String[] args) {
@@ -10,4 +12,28 @@ public class BaseClass {
 	public void palindromeByGanesh() {
 
 	}
+	
+	private void PalindromeByGopi() {
+		String original, reverse = ""; // Objects of String class
+	      Scanner ab = new Scanner(System.in);
+	     
+	      System.out.println("Check entered string is a palindrome or not");
+	      original = ab.nextLine();
+	     
+	      int length = original.length();
+	     
+	      for (int i = length - 1; i >= 0; i--)
+	         reverse = reverse + original.charAt(i);
+	         
+	      if (original.equals(reverse))
+	         System.out.println("The string is a palindrome.");
+	      else
+	         System.out.println("The string isn't a palindrome.");
+	         
+	} 
+	
+	
+	
+	
+	
 }
