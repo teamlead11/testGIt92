e70bd9d7bb6115b2a8c46f469dd77f68a86e9208
Shankar0302 <sankar03m@gmail.com>
2018-11-23 10:03:35 +0530

    palindrome was changed

--- src/mypack/BaseClass.java
+++ src/mypack/BaseClass.java
@@ -53,5 +53,28 @@ public static void PalindromeRanjitha() {
        System.out.println("The string is a palindrome.");
     else
        System.out.println("The string isn't a palindrome.");
-}    
+}  
+// Shankar
+
+public void palindromeShan() {
+
+    String a, b = "";
+    Scanner s = new Scanner(System.in);
+    System.out.print("Enter the string you want to check:");
+    a = s.nextLine();
+    int n = a.length();
+    for(int i = n - 1; i >= 0; i--)
+    {
+        b = b + a.charAt(i);
+    }
+    if(a.equalsIgnoreCase(b))
+    {
+        System.out.println("The string is palindrome.");
+    }
+    else
+    {
+        System.out.println("The string is not palindrome.");
+    }
+
+}
  }
