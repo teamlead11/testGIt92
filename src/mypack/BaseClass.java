64003449c065a9e7930187bf35fbf7a8bea65827
manivelsele <rajamanivel@gmail.com>
2018-11-23 09:24:48 +0530

    Pali added by manivel

--- src/mypack/BaseClass.java
+++ src/mypack/BaseClass.java
@@ -35,5 +35,26 @@ public class BaseClass {
 	         
 	      else {
 	         System.out.println("The string isn't a palindrome.");
-	      }}
+	      }
+	      
+	}
+	
+	public void paLindromeByManivel() {
+		String rev="";
+		System.out.println("Enter the string");
+		Scanner aal = new Scanner(System.in);
+		String name = aal.next();
+		int j=name.length();
+		System.out.println(j);
+		for (int i = j-1; i >=0; i--) {
+			rev=rev+name.charAt(i);
+		}
+		if (name.equals(rev)) {
+			System.out.println("String is palindrome");
+		}
+		else {
+			System.out.println("String is not palindrome");
+		}
+	}
+	
 }
