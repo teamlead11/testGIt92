1ad6cf715cf28a09f4b80ddef34a0b75719403c5
Muthugsm <sellamuthu95@gmail.com>
2018-11-23 10:12:06 +0530

    my palindrome added

--- src/mypack/BaseClass.java
+++ src/mypack/BaseClass.java
@@ -53,5 +53,31 @@ public static void PalindromeRanjitha() {
        System.out.println("The string is a palindrome.");
     else
        System.out.println("The string isn't a palindrome.");
-}    
+}  
+
+
+
+public static void palindrome_muthu_gsm() {
+	// TODO Auto-generated method stub
+
+
+	String s, rev = "";
+      Scanner in = new Scanner(System.in);
+     
+      System.out.println("Enter a string");
+      s = in.nextLine();
+     
+      int length = s.length();
+     
+      for (int i = length - 1; i >= 0; i--) {
+         rev = rev + s.charAt(i);
+      }
+      if (s.equals(rev)) {
+    	  System.out.println("The string is a palindrome.");  
+      }
+         
+      else {
+         System.out.println("The string isn't a palindrome.");
+      }
+}
  }
