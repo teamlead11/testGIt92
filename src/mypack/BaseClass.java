47acd0aa91db0a95d2929e24ed10ee1c731a0965
dkdeepakkumar3345 <dkdeepakkumar766@gmail.com>
2018-12-04 14:36:39 +0530

    added new testcase

--- src/mypack/BaseClass.java
+++ src/mypack/BaseClass.java
@@ -12,25 +12,6 @@ public class BaseClass {
 	public void palindromeByGanesh() {
 
 	}
-
-	public void paLindromeByDeepak() {
-		String rev="";
-		System.out.println("Enter the string");
-		Scanner aal = new Scanner(System.in);
-		String name = aal.next();
-		int j=name.length();
-		System.out.println(j);
-		for (int i = j-1; i >=0; i--) {
-			rev=rev+name.charAt(i);
-		}
-		if (name.equals(rev)) {
-			System.out.println("String is palindrome");
-		}
-		else {
-			System.out.println("String is not palindrome");
-		}
-		
-	}	
 	
 
 	public void palindromeBySriram(){
@@ -56,6 +37,25 @@ public class BaseClass {
 	      else {
 	         System.out.println("The string isn't a palindrome.");
 	      }	}
+	
+	public void paLindromeByDeepak() {
+		String rev="";
+		System.out.println("Enter the string");
+		Scanner aal = new Scanner(System.in);
+		String name = aal.next();
+		int j=name.length();
+		System.out.println(j);
+		for (int i = j-1; i >=0; i--) {
+			rev=rev+name.charAt(i);
+		}
+		if (name.equals(rev)) {
+			System.out.println("String is palindrome");
+		}
+		else {
+			System.out.println("String is not palindrome");
+		}
+		
+	}	
 
 public static void PalindromeRanjitha() {
     String original, reverse = ""; 
